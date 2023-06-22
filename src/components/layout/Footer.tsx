314937e3b767b68451a8dda9a3a7e53b795e6a0d
Goran Markovic <goranmarkovic2008@gmail.com>
2023-06-22 21:01:54 +0200

    modified footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,28 +2,24 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-t-lg shadow dark:bg-gray-900 mt-16 sm:px-36 px-12">
+    <footer className="bg-white rounded-t-lg shadow dark:bg-gray-900 mt-16 sm:px-36 px-12 py-2 sm:py-0">
       <div className="w-full container mx-auto p-4 md:px-6 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <ul className="flex sm:flex-row flex-col flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex sm:flex-row gap-2 sm:gap-0 flex-col flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className=" hover:underline sm:mr-6 ">
                 About
               </a>
             </li>
+           
             <li>
-              <a href="#" className=" hover:underline sm:mr-6">
-                Privacy Policy
+              <a href="https://github.com/goran010" className=" hover:underline sm:mr-6 ">
+                GitHub
               </a>
             </li>
             <li>
-              <a href="#" className=" hover:underline sm:mr-6 ">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
+              <a href="https://www.linkedin.com/in/goran-markovi%C4%87-b2662920a/" className="hover:underline">
+                LinkedIn
               </a>
             </li>
           </ul>
@@ -31,10 +27,9 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+          <a href="https://github.com/goran010/weather-app" className="hover:underline">
+            Weather app - Goran Marković
           </a>
-          . All Rights Reserved.
         </span>
       </div>
     </footer>
