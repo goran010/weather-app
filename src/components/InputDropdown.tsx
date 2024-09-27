6c9a79b9370ac2e6379198cb76a43dd2d4d7fa0e
Goran Marković <91391493+goran010@users.noreply.github.com>
2024-09-27 12:53:34 +0200

    added .env variables

--- src/components/InputDropdown.tsx
+++ src/components/InputDropdown.tsx
@@ -1,85 +1,72 @@
-//hooks
-import React, { useEffect, useState } from "react";
-
-//axios
+import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
-
-//flag icons
 import { CircleFlag } from "react-circle-flags";
-
-//interfaces
-import { InputDropdownProps } from "../Models/ModelsList";
-import { CityLocationData } from "../Models/ModelsList";
+import { InputDropdownProps, CityLocationData } from "../Models/ModelsList";
 
 const InputDropdown: React.FC<InputDropdownProps> = ({
   searchedName,
   changeCity,
 }) => {
   const BASE_METEO_API_URL = process.env.REACT_APP_BASE_METEO_API_URL;
-
   const [cities, setCities] = useState([] as CityLocationData[]);
 
-  //get cities that match searched city
-  const fetchNames = async (cityName: string) => {
-    await axios
-      .get(`${BASE_METEO_API_URL}?name=${cityName}&count=5`)
-      .then((response) => {
+  // Memoize fetchNames to avoid unnecessary recreation on each render
+  const fetchNames = useCallback(
+    async (cityName: string) => {
+      try {
+        const response = await axios.get(
+          `${BASE_METEO_API_URL}?name=${cityName}&count=5`
+        );
         setCities(response.data.results);
-      })
-      .catch((err) => console.error(err));
-  };
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [BASE_METEO_API_URL]
+  );
 
-  //fetch new cities when searched term is changed
+  // fetch new cities when searched term is changed
   useEffect(() => {
     fetchNames(searchedName);
-  }, [searchedName]);
+  }, [searchedName, fetchNames]);
 
   return (
     <ul className="bg-white min-w-max border-slate-200 border-2 rounded-md absolute w-full top-0 pt-10 text-black">
-      {cities ? (
-        cities.map((item) => {
-          return (
-            <li
-              key={item.id}
-              className="cursor-pointer min-w-max bg-white p-2 pl-3 pb-2  p-w-3 hover:bg-slate-200 flex gap-1"
-              onClick={() => {
-                changeCity({
-                  lat: item.latitude,
-                  lon: item.longitude,
-                  cityName: item.name,
-                  countryName: item.country,
-                  countryCode: item.country_code.toLowerCase(),
-                });
-              }}
-            >
-              <CircleFlag
-                countryCode={item.country_code.toLowerCase()}
-                className="h-6"
-              />
-              <p className="font flex flex-col sm:flex-row">
-                <span>
-                  {" "}
-                  {`
+      {cities.length ? (
+        cities.map((item) => (
+          <li
+            key={item.id}
+            className="cursor-pointer min-w-max bg-white p-2 pl-3 pb-2  p-w-3 hover:bg-slate-200 flex gap-1"
+            onClick={() => {
+              changeCity({
+                lat: item.latitude,
+                lon: item.longitude,
+                cityName: item.name,
+                countryName: item.country,
+                countryCode: item.country_code.toLowerCase(),
+              });
+            }}
+          >
+            <CircleFlag
+              countryCode={item.country_code.toLowerCase()}
+              className="h-6"
+            />
+            <p className="font flex flex-col sm:flex-row">
+              <span>
+                {`
               ${item.name}, 
               ${item.admin1 ? `${item.admin1},` : "("} 
               `}
-                </span>
-                <span>{`
-              ${
-                item.latitude.toFixed(2)
-                  ? ` (${item.latitude.toFixed(2)}°E ,`
-                  : ""
-              }   
-              ${
-                item.longitude.toFixed(2)
-                  ? `${item.latitude.toFixed(2)}°N  ,`
-                  : ""
-              } 
-              ${item.elevation ? `${item.elevation}m ` : ""})`}</span>
-              </p>
-            </li>
-          );
-        })
+              </span>
+              <span>
+                {`
+              ${item.latitude.toFixed(2)}°E ,   
+              ${item.longitude.toFixed(2)}°N, 
+              ${item.elevation ? `${item.elevation}m ` : ""})`}
+              </span>
+            </p>
+          </li>
+        ))
       ) : (
         <li className="cursor-pointer">No cities found</li>
       )}
